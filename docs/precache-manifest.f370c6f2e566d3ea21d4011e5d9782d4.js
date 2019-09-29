24e41381ecba738dd191602886b8360704fef1fe
dgatto <dgattoiii@gmail.com>
2019-09-29 19:03:24 -0400

    new build

---
+++ docs/precache-manifest.f370c6f2e566d3ea21d4011e5d9782d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8774e9c42e596f55c43cc6014aacda5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f50a777ce1fd8dd27f9",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f6d55958b239fec8252d",
+    "url": "/static/css/main.1a93eb97.chunk.css"
+  },
+  {
+    "revision": "7f50a777ce1fd8dd27f9",
+    "url": "/static/js/2.7a2144e4.chunk.js"
+  },
+  {
+    "revision": "f6d55958b239fec8252d",
+    "url": "/static/js/main.90313b3f.chunk.js"
+  },
+  {
+    "revision": "332f673067c53b42dba2",
+    "url": "/static/js/runtime-main.bc2b2045.js"
+  }
+]);
